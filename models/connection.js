75ad46cca51db20a9c8937d70dbab61e0957bfc8
roberto <robertonnv@gmail.com>
2021-10-28 00:58:30 -0300

    desafio

--- models/connection.js
+++ models/connection.js
@@ -1,20 +1,22 @@
-const mongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-const DB_NAME = 'StoreManager';
+const DB = 'StoreManager';
+
+const URL = 'mongodb://mongodb:27017/StoreManager';
 
 const OPTIONS = {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 };
 
 const connection = async () => {
-  return await mongoClient.connect(MONGO_DB_URL, OPTIONS)
-    .then((conn) => conn.db(DB_NAME))
+  return await MongoClient.connect(URL, OPTIONS)
+    .then((conn) => conn.db(DB))
     .catch((err) => {
-      console.error(err);
-      process.exit(1);
-    });
+      console.log(err);
+      process.exit();
+    }
+    );
 };
 
 module.exports = connection;
