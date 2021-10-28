20c6c7e025905c04ac69a3fb65dccab1f9eb9ef4
roberto <robertonnv@gmail.com>
2021-10-28 01:03:05 -0300

    desafio

--- models/connection.js
+++ models/connection.js
@@ -6,17 +6,16 @@ const URL = 'mongodb://mongodb:27017/StoreManager';
 
 const OPTIONS = {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 };
 
 const connection = async () => {
-  return await MongoClient.connect(URL, OPTIONS)
+   await MongoClient.connect(URL, OPTIONS)
     .then((conn) => conn.db(DB))
     .catch((err) => {
       console.log(err);
       process.exit();
-    }
-    );
+    },);
 };
 
 module.exports = connection;
