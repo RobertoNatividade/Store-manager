b5e211ef2a96503051cd6ebc7cb7502c4ac458f9
roberto <robertonnv@gmail.com>
2021-10-31 08:26:46 -0300

    desafio

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser').json();
+
+const productsRoutes = require('./routes/productsRoutes');
+const salesRoutes = require('./routes/salesRoutes');
+const { handleError } = require('./middlewares');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser);
+
+app.use('/products', productsRoutes);
+app.use('/sales', salesRoutes);
+app.use('/', handleError);
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.send();
+});
+
+app.listen(PORT, () => {
+  console.clear();
+  console.log(`Funcionando na porta ${PORT}`);
+});
